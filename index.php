799a59298027b5208fe2c656e90924f3cb0808cf
ODDpavel <rotanov.pavel@gmail.com>
2017-07-25 18:17:00 +0300

    h1 was added

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <title> MyDev </title>
+</head>
+<body>
+    <h1> MyFirstDev</h1>
+</body>
+</html>
